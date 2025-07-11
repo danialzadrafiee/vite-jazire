965a42ee0981ed5a565fb6586c0a7909fe2bc310
danialzadrafiee <leylasarafrezai@gmail.com>
2025-07-11 16:42:46 +0330

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   server: {
-    port: 517310,
+    port: 5270,
     allowedHosts: true
   }
 })
